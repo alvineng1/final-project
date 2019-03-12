5105bd7237de62f4c0dcfddc699b90449fb2989e
Steven Hsieh <hsiehsc@uw.edu>
2019-03-12 16:37:54 -0700

    Split by sex

--- generation/generation/ui.R
+++ generation/generation/ui.R
@@ -6,15 +6,16 @@ shinyUI(navbarPage(
   # Creating tab panel
   tabPanel(
     "Generation",
-    titlePanel("Something by generation"),
+    titlePanel("Suicides by sex and generation"),
     # Creating sidebar layout
     sidebarLayout(
       # Sidebar Panel
       sidebarPanel(
         # Input to select variable to map
-        selectInput('text', "Select Country", country_list, selectize=TRUE),
+        selectInput('country_text_name', "Select Country", country_list, selectize=TRUE),
         sliderInput("slider_year", label = h3("Select Year"), min = 1985, 
-                    max = 2016, value = 2000, sep = "")
+                    max = 2016, value = 2000, sep = ""),
+        checkboxInput("gender_checkbox", label = "Show Sex", value = TRUE)
       ),
       # Main Panel to Show Scatter Plot
       mainPanel(
